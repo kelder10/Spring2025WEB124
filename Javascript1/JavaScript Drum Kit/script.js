afff05d7a6120368b0ba9e9216aafc45d7903339
kelder10 <klang5875@gmail.com>
2025-03-14 06:11:18 -0700

    Update script.js

--- Javascript1/JavaScript Drum Kit/script.js
+++ Javascript1/JavaScript Drum Kit/script.js
@@ -11,38 +11,37 @@
 // - Prevented multiple sounds from playing simultaneously.
 
 function removeTransition(e) {
-// removeTransition: Removes the 'playing' class after a CSS transition ends
-if (e.propertyName !== 'transform') return;
-e.target.classList.remove('playing');
+// Removes the 'playing' class after a CSS transition ends
+  if (e.propertyName !== 'transform') return;
+  e.target.classList.remove('playing');
 }
 
 function playSound(e) {
-// playSound: Plays the sound associated with the key pressed or clicked.
-// It also resets other sounds to ensure only one sound plays at a time.
-const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
-const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
-
-if (!audio) return;
-
-// Stop other sounds before playing the selected one
-const allAudios = document.querySelectorAll('audio');
-allAudios.forEach((otherAudio) => {
-otherAudio.pause();
-otherAudio.currentTime = 0; // Reset the sound to the start
-});
-
-key.classList.add('playing');
-audio.currentTime = 0; // Reset the audio
-audio.play(); // Play the selected sound
+  // Plays the sound associated with the key pressed or clicked
+  const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+  const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
+  
+  if (!audio) return;
+  
+  // Stop other sounds before playing the selected one
+  const allAudios = document.querySelectorAll('audio');
+  allAudios.forEach((otherAudio) => {
+  otherAudio.pause();
+  otherAudio.currentTime = 0; // Reset the sound to the start
+  });
+  
+  key.classList.add('playing');
+  audio.currentTime = 0; // Reset the audio
+  audio.play(); // Play the selected sound
 }
 
 // Allow clicking on animal keys to play sounds
 const keys = Array.from(document.querySelectorAll('.key'));
 keys.forEach(key => {
-key.addEventListener('click', () => {
-const keyCode = key.dataset.key;
-window.dispatchEvent(new KeyboardEvent('keydown', {'keyCode': keyCode}));
-});
+    key.addEventListener('click', () =>{
+    const keyCode = key.dataset.key;
+    window.dispatchEvent(new KeyboardEvent('keydown', {'keyCode': keyCode}));
+  }); 
 });
 
 // Add event listeners for playing sounds with keyboard
