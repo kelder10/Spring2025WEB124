564a87adf4875a60784792aa8dd24ccd0af89f09
kelder10 <klang5875@gmail.com>
2025-04-25 10:14:29 -0700

    Update script.js

--- JS Calculator/script.js
+++ JS Calculator/script.js
@@ -1,1 +1,8 @@
-
+function add() {
+const num = parseFloat(document.getElementById('num').value);
+let result = '';
+for (let i = 1; i <= 10; i++) {
+result += `${i} + ${num} = ${i + num}\n`;
+}
+document.getElementById('addition').innerText = result.trim();
+}
